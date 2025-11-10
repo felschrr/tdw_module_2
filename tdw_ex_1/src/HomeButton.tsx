9fa9f729fcc405fea29109cb8742202297ebba51
felschrr <101103880+felschrr@users.noreply.github.com>
2025-11-10 11:04:59 +0000

    Delete tdw_ex_1 directory

--- tdw_ex_1/src/HomeButton.tsx
+++
@@ -1,9 +0,0 @@
-const HomeButton = () => {
-  return (
-    <>
-      <a href="/" className="px-3 py-2 bg-white border rounded-full border-stone-200 hover:bg-stone-200 hover:border-stone-300 hover:text-blue-500">←</a>
-    </>
-  );
-};
-
-export default HomeButton;
